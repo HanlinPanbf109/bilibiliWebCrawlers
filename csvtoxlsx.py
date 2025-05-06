fd091e221db5011fcfdd0df67e893d16e608cb33
Pan Hanlin <3512688241@qq.com>
2025-05-06 11:36:32 +0800

    Update csvtoxlsx.py

--- csvtoxlsx.py
+++ csvtoxlsx.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 # 读取CSV文件
-df = pd.read_csv('./result/直升机到底安全吗.csv')#替换为文件名
+df = pd.read_csv('')#替换为文件名
 
 #转化csv文件到xlsx
-xlsx_file='./result/直升机到底安全吗.xlsx'
+xlsx_file='.xlsx'
 df.to_excel(xlsx_file,index=False)      
